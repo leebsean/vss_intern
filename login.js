1a632751e9ed5067d15ff5deae17d232c9093726
leebsean <lbsean116@gmail.com>
2024-07-09 17:27:16 +0700

    abc

---
+++ login.js
@@ -0,0 +1,9 @@
+document.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const submit = document.getElementById('test');
+    if (test) {
+        console.log(test.innerHTML);
+    } else {
+        console.log("Element not found.");
+    }
+});
