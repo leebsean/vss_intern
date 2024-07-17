d023572345cb58a1cfba406b2bc8eba18912c789
leebsean <lbsean116@gmail.com>
2024-07-17 14:48:14 +0700

    abc

--- js/login.js
+++ js/login.js
@@ -1,9 +1,28 @@
-document.addEventListener('submit', (event) => {
-    event.preventDefault(); 
-    const submit = document.getElementById('test');
-    if (test) {
-        console.log(test.innerHTML);
+
+function add(a ,b){
+    let res = a+b;
+    return res;
+}
+//let val = add(2, 3);
+//console.log(add(2, 3));
+
+function evenOdd(x){
+    if(x % 2 == 0){
+        console.log(x + " is an even number.");
     } else {
-        console.log("Element not found.");
+        console.log(x + " is an odd number.");
     }
-});
+}
+
+//evenOdd(4);
+
+let nums = new Array();
+const nums2 = [40];
+nums2[5] = 24;
+console.log(nums2[5]);
+
+function isValidEmail(email){
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
+}
+console.log(isValidEmail("son@gmail.comu"));
